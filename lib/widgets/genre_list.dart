425e8acc15ff394236452d345de5d2f07cd5c7dc
Argha Niqie <niqiewijaksono1106@gmail.com>
2026-09-24 01:08:47 +0700

    feat: menambahkan widget list genre

---
+++ lib/widgets/genre_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class GenreList extends StatelessWidget {
+  final String selected;
+  final ValueChanged<String>? onGenreSelected;
+  const GenreList({super.key, this.selected = "All", this.onGenreSelected});
+
+  final List<String> genres = const ["All", "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Sci-Fi"];
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04, vertical: screenHeight * 0.01),
+      child: Row(
+        children: genres.map((genre) {
+          final isActive = genre == selected;
+          return Padding(
+            padding: EdgeInsets.only(right: screenWidth * 0.04),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(screenWidth * 0.04),
+              onTap: () {}, // TODO: Update selected genre
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04, vertical: screenHeight * 0.01),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(screenWidth * 0.04),
+                  color: isActive ? Colors.blue : const Color(0xFF0b395e),
+                  boxShadow: [
+                    BoxShadow(color: Colors.black.withValues(alpha: 0.3), blurRadius: screenWidth * 0.02, offset: Offset(0, screenHeight * 0.005)),
+                  ],
+                ),
+                child: Text(genre, style: TextStyle(fontSize: screenWidth * 0.04, color: isActive ? Colors.white : Colors.grey.shade400, fontWeight: FontWeight.bold)),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
